1b3aa689e12166dd3c8b4e08d538f7345b858dba
Brian Drawert <bdrawert@cs.ucsb.edu>
2012-12-12 10:53:17 -0800

    updated matlab path script to remove previous installs of urdme

--- set_matlab_path.pl
+++ set_matlab_path.pl
@@ -9,9 +9,21 @@ if(!defined $URDME_ROOT){
     exit(0);
 }
 
+
+##filename = which('urdme');
+##while(~isempty(filename))
+##[pathstr, name, ext] = fileparts(filename);
+##rmpath(pathstr);
+##filename = which('urdme');
+##end
+
+my $cleanup_cmd = "filename = which('urdme');while(~isempty(filename)),[pathstr, name, ext] = fileparts(filename);rmpath(pathstr);filename = which('urdme');end";
+
+
 my $in = gensym;
 my $MATLAB_CMD = 'matlab -nojvm -nosplash';
 open($in, '|'.$MATLAB_CMD);
+syswrite($in,"$cleanup_cmd\n\n");
 syswrite($in,"addpath $URDME_ROOT/msrc\n");
 syswrite($in,"savepath\n");
 sleep(2);
