7e6012424b707283272bd83e1d1b7e63d9d348e8
Stefan Engblom <stefane@it.uu.se>
2017-03-15 10:48:54 +0100

    meta info + new startup.m

---
+++ startup.m
@@ -0,0 +1,10 @@
+%STARTUP Startup for URDME.
+
+% S. Engblom 2017-02-14
+
+% link = location of this startup.m
+link = mfilename('fullpath');
+link = link(1:end-7); % s-t-a-r-t-u-p is 7 chars
+
+% path to urdme/ folders
+addpath(genpath([link 'urdme/']));
