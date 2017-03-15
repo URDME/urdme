0702bbfe08757167aff63ca641621e5a857888d8
Stefan Engblom <stefane@it.uu.se>
2017-03-15 11:08:01 +0100

    new comsol interface

--- urdme/comsol/comsol2urdme.m
+++ urdme/comsol/comsol2urdme.m
@@ -1,343 +1,150 @@
-function umod = comsol2urdme(fem,varargin)
-%COMSOL2URDME Create an URDME-structure from a COMSOL multiphysics model
-%   UMOD = COMSOL2URDME(FEM,VARARGIN)
+function umod = comsol2urdme(fem,verbose)
+%COMSOL2URDME Create URDME-structure from a COMSOL multiphysics model.
+%   UMOD = COMSOL2URDME(COMSOL,VERBOSE) creates an URDME-structure
+%   UMODE from the Comsol Java object COMSOL under the verbosity
+%   VERBOSE.
 %
-%   --INPUT--
-%   FEM - Comsol Multiphysics 3.5a or 4.x model file
-%   VERBOSE - Silent execution
-%
-%   --OUTPUT--
-%   UMOD - Urdme 1.2 structure with fem-model added to umod.comsol
-%
-%   Generalized subdomains (non-empty argument SDUSED). 
-%   The generalized subdomain vector is given a default value of one. Other 
-%   values can be defined by a special expression (rdme_sd) which should be 
-%   defined on every subdomain, boundary and point. The global expression 
-%   rdme_sdlevel should be defined as the lowest dimension that rdme_sd is 
-%   defined for.
-%
+%   See also URDME, URDME2COMSOL.
 
-% P. Bauer     2012-08-30
-% V. Gerdin    2012-02-08 (mod2rdme)
-% S. Engblom   2008-08-01 (fem2rdme)
-% A. Hellander 2009-11-18 (fem2rdme)
-is4x = isjava(fem);
-if nargin > 2
-    verbose = varargin{1};
-else
-    verbose = 0;
+% S. Engblom 2017-02-15 (Major revision, URDME 1.3, Comsol 5)
+% P. Bauer 2012-08-30 (Revision)
+% V. Gerdin 2012-02-08 (mod2rdme)
+% S. Engblom 2008-08-01 (fem2rdme)
+
+% Generalized subdomains (non-empty argument SDUSED). The generalized
+% subdomain vector is given a default value of one. Other values can
+% be defined by a special expression (rdme_sd) which should be defined
+% on every subdomain, boundary and point. The global expression
+% rdme_sdlevel should be defined as the lowest dimension that rdme_sd
+% is defined for.
+
+if nargin < 2, verbose = 0; end
+
+% Get extended mesh data
+l_info(verbose,1,'Start mphxmeshinfo...');
+xmi = mphxmeshinfo(fem); 
+l_info(verbose,1,' ...done.\n');
+
+% Get number of species and number of cells.
+[Mspecies,Ncells] = size(xmi.nodes.dofs);
+Ndofs = Ncells*Mspecies;
+if Ndofs ~= xmi.ndofs
+  warning(['Number of degrees of freedoms mismatch. ' ...
+           'Check the boundary conditions.']);
 end
+% permutation of species
+[foo,sp] = ismember(xmi.fieldnames,xmi.nodes.dofnames);
 
-if is4x  
-  if verbose>0    
-    fprintf('Identified fem-model as Comsol 4.x.\n');
-  end
-      
-  % Get extended mesh data
-  if verbose>0
-    disp('Start mphxmeshinfo')
-  end
+% permutation comsol -> urdme
+f2r = reshape(1:Ndofs,Mspecies,Ncells);
+f2r = reshape(f2r(sp,:),[ ],1);
 
-  xmi = mphxmeshinfo(fem); 
-  if verbose>0
-    disp('Done')
-  end
+% Assemble matrices
+l_info(verbose,1,'Start mphmatrix...');
+DM = mphmatrix(fem,'sol1','out',{'K','D'});
+l_info(verbose,1,' ...done.\n');
+D = DM.K; % URDME: D "Diffusion matrix" Comsol: K "Stiffness matrix"
+M = DM.D; % URDME: M "Mass matrix"      Comsol: D "Damping matrix"
 
-  % Get number of species and number of cells.
-  [Mspecies,Ncells] = size(xmi.nodes.dofs);
-  Ndofs = Ncells*Mspecies;
-  if Ndofs ~= xmi.ndofs
-      warning(['Number of degrees of freedoms mismatch. ' ...
-               'Check the boundary conditions.']);
-  end
-  % permutation of species
-  [foo,sp] = ismember(xmi.fieldnames,xmi.nodes.dofnames);
+% The (lumped) mass matrix gives the element volume
+vol = full(sum(M,2));
 
-  % permutation fem -> rdme
-  f2r = reshape(1:Ndofs,Mspecies,Ncells);
-  f2r = reshape(f2r(sp,:),[ ],1);
+% explicitly invert the lumped mass matrix and filter the diffusion matrix
+[i,j,s] = find(D);
+diagsum1 = sum(diag(D)./vol);
+s = -s./vol(i);
+ixkeep = find(s > 0);
+i = i(ixkeep); j = j(ixkeep); s = s(ixkeep);
 
-  % Create tspan & u0 placeholder, must be redefined in model.m
-  tspan = zeros(1,0);
-  u0 = zeros(1,0);
+% permute and rebuild
+if2r(f2r) = 1:Ndofs;
+[i,j,p] = find(sparse(if2r(i),if2r(j),1:numel(s)));
+D = sparse(i,j,s(p),Ndofs,Ndofs);
+d = full(sum(D,2));
+D = D+sparse(1:Ndofs,1:Ndofs,-d);
+diagsum2 = sum(d);
 
-  % Assemble matrices
-  if verbose>0
-    disp('Start mphmatrix')
-  end
-  DM = mphmatrix(fem,'sol1','out',{'K','D'});
-  if verbose>0
-    disp('mphmatrix: Done')
-  end
-  D = DM.K; % URDME: D "Diffusion matrix"  Comsol: K "Stiffness matrix"
-  M = DM.D; % URDME: M "Mass matrix"  Comsol: D "Damping matrix"
+% check if the difference is too big
+if abs(diagsum2-diagsum1) > abs(diagsum1)*0.10
+  warning('Many off diagonal negative elements in D.');
+end
+% need only volume of unique cells
+vol = vol(f2r);
+vol = vol(1:Mspecies:end);
 
-  % The (lumped) mass matrix gives the element volume
-  vol = full(sum(M,2));
+% Generalized subdomains. To support more than one subdomain in the
+% models, the global expression "urdme_sdlevel" has to be defined.
+sd = ones(1,Ncells);
 
-  % explicitly invert the lumped mass matrix and filter the diffusion matrix
-  [i,j,s] = find(D);
-  diagsum1 = sum(diag(D)./vol);
-  s = -s./vol(i);
-  ixkeep = find(s > 0);
-  i = i(ixkeep); j = j(ixkeep); s = s(ixkeep);
+% Find rdme_sdlevel value if present
+urdme_sdlvl = -1;
+l_info(verbose,1,'Start mphglobal...')
+try
+  urdme_sdlvl = mphglobal(fem,'urdme_sdlevel','solnum','end');
+catch
+  l_info(verbose,1,['Global variable "urdme_sdlevel" not set. ' ...
+                    'No subdomains will be used.']);
+end
+l_info(verbose,1,' ...done.\n')
 
-  % permute and rebuild
-  if2r(f2r) = 1:Ndofs;
-  [i,j,p] = find(sparse(if2r(i),if2r(j),1:numel(s)));
-  D = sparse(i,j,s(p),Ndofs,Ndofs);
-  d = full(sum(D,2));
-  D = D+sparse(1:Ndofs,1:Ndofs,-d);
-  diagsum2 = sum(d);
+% if we found a valid urdme_sdlvl initial sd values, set all to 0
+if urdme_sdlvl > -1
+  sd = zeros(1,Ncells);
 
-  % check if the difference is too big
-  if abs(diagsum2-diagsum1) > abs(diagsum1)*0.10
-    warning('Many off diagonal negative elements in D.');
-  end
-  % need only volume of unique cells
-  vol = vol(f2r);
-  vol = vol(1:Mspecies:end);
-
-  % Generalized subdomains. To support more than one subdomain in 
-  % the models, the global expression "rdme_sdlevel" has to 
-  % be defined.
-
-  sd = ones(1,Ncells);
-
-  % Find rdme_sdlevel value if present
-  rdme_sdlvl = -1;
-  if verbose>0
-    disp('Start mphglobal')
-  end
-  try
-      rdme_sdlvl = mphglobal(fem,'rdme_sdlevel','solnum','end');
-  catch
-      warning('Global variable "rdme_sdlevel" not set. No subdomains will be used.');
-  end
-  if verbose>0
-    disp('Done')
-  end
-
-  if rdme_sdlvl > -1 % i.e. if we found a valid rdme_sdlvl
-    % initial sd values, set all to 0
-    sd = zeros(1,Ncells);
-
-    % sdim - the maximum number of dimensions to run simulation
-    sdim = length(xmi.elements.meshtypes)-1;
-    % edim2str - translation from numeric edim to string
-    edim2str = {'vtx' 'edg' 'tri' 'tet'};
-
-    for edim = sdim:-1:rdme_sdlvl
-
-      % enode_idx - the indices of the nodes involved for a given edim
-      enode_idx = unique(xmi.elements.(edim2str{edim+1}).nodes+1);
-      % add 1 to every index as 4.2 indices start from 0
-
-      % coords - x,y,z coordinates of each involved node 
-      coords = xmi.nodes.coords(:,enode_idx);
-      if verbose>0
-        disp('Start mphinterp')
-      end
-      try        
-        rdme_sd = mphinterp(fem, 'rdme_sd', 'coord', coords, 'edim', edim, 'solnum', 1);
-      catch
-          error(['To use more than une subdomain, you need to specify' ...
-                 'the expression rdme_sd, see manual.'])
-      end
-      if verbose>0
-        disp('Done')
-      end
-      % Set sd for involved nodes
-      sd(enode_idx) = rdme_sd;
-
-    end
-    % sd is currently ordered according to comsol nodes. We need to permute
-    %     it to be ordered according to comsol dofs (like everything else).
-    sd = sd(xmi.dofs.nodes(1:Mspecies:end)+1);
+  % sdim - the maximum number of dimensions to run simulation
+  sdim = length(xmi.elements.meshtypes)-1;
+  % edim2str - translation from numeric edim to string
+  edim2str = {'vtx' 'edg' 'tri' 'tet'};
+  for edim = sdim:-1:urdme_sdlvl
+    % enode_idx - the indices of the nodes involved for a given edim
+    enode_idx = unique(xmi.elements.(edim2str{edim+1}).nodes+1);
     % add 1 to every index as 4.2 indices start from 0
-  end
 
-  % as of the new Comsol API the species' names are refered to as
-  % "modelID.speciesName", ex "mod1.MinD_c_atp". The following code
-  % removes the new prefix
-  tags = fem.modelNode.tags;
-  species = xmi.fieldnames;
-
-  for i = [1:size(xmi.fieldnames)]
-      if strfind(xmi.fieldnames{i},[char(tags(1)) '.']) == 1
-          fullName = char(xmi.fieldnames{i});
-          species{i} = fullName(size(char(tags(1)),2)+2:end);
-      end
+    % coords - x,y,z coordinates of each involved node 
+    coords = xmi.nodes.coords(:,enode_idx);
+    l_info(verbose,1,'Start mphinterp...');
+    try        
+      urdme_sd = mphinterp(fem,'urdme_sd','coord',coords,'edim',edim,'solnum',1);
+    catch
+      error(['To use more than une subdomain, you need to specify ' ...
+             'the expression urdme_sd, see manual.'])
+    end
+    l_info(verbose,1,' ...done.\n');
+    % Set sd for involved nodes
+    sd(enode_idx) = urdme_sd;
   end
-      
-      
-else
-   if verbose>0    
-    fprintf('Identified fem-model as Comsol 3.5a.\n');
-  end
-      
-  % extend mesh
-  if verbose>0
-    disp('Start meshextend')
-  end
-  try
-  fem.xmesh = meshextend(fem);
-  catch
-      umod=fem;
-      return;
-  end
-  if verbose>0
-    disp('Done')
-  end
-
-  % fundamentals
-  if verbose>0
-    disp('Start xmeshinfo')
-  end
-  [nodes,dofs] = xmeshinfo(fem.xmesh,'out',{'nodes' 'dofs'});
-  if verbose>0
-    disp('Done')
-  end
-  [Mspecies,Ncells] = size(nodes.dofs);
-  Ndofs = Ncells*Mspecies;
-  if Ndofs ~= flngdof(fem)
-    warning(['Number of degrees of freedoms mismatch. ' ...
-            'Check the boundary conditions.']);
-  end
-  % permutation of species
-  [foo,sp] = ismember(fem.equ.dim,nodes.names);
-
-  % permutation fem -> rdme
-  f2r = reshape(1:Ndofs,Mspecies,Ncells);
-  f2r = reshape(f2r(sp,:),[ ],1);
-
-  % create tspan (if any)
-  tspan = zeros(1,0);
-  if isfield(fem,'sol') && isfield(fem.sol,'tlist')
-    tspan = fem.sol.tlist;
-  end
-
-  % u0
-  if verbose>0
-    disp('Start asseminit')
-  end
-  u0 = asseminit(fem);
-  if verbose>0
-    disp('Done')
-  end
-  u0 = reshape(u0.u(f2r),Mspecies,Ncells);
-
-  % assemble matrices
-  if verbose>0
-    disp('Start assemble')
-  end
-  [D,M] = assemble(fem,'out',{'K','D'});
-  if verbose>0
-    disp('Done')
-  end
-
-  % the (lumped) mass matrix gives the element volume
-  vol = full(sum(M,2));
-
-  % explicitly invert the lumped mass matrix and filter the diffusion matrix
-  [i,j,s] = find(D);
-  diagsum1 = sum(diag(D)./vol);
-  s = -s./vol(i);
-  ixkeep = find(s > 0);
-  i = i(ixkeep); j = j(ixkeep); s = s(ixkeep);
-
-  % permute and rebuild
-  if2r(f2r) = 1:Ndofs;
-  [i,j,p] = find(sparse(if2r(i),if2r(j),1:numel(s)));
-  D = sparse(i,j,s(p),Ndofs,Ndofs);
-  d = full(sum(D,2));
-  D = D+sparse(1:Ndofs,1:Ndofs,-d);
-  diagsum2 = sum(d);
-
-  % check if the difference is too big
-  if abs(diagsum2-diagsum1) > abs(diagsum1)*0.10
-    warning('Many off diagonal negative elements in D.');
-  end
-
-  % switch to absolute numbers
-  u0real = 6.022e23*vol'*u0(:);
-  u0 = round(6.022e23*reshape(vol,Mspecies,Ncells).*u0);
-
-  % check if the difference is too big
-  if abs(u0real-sum(u0(:))) > u0real*1e-2
-    warning('The initial concentration is not accurately represented.');
-  end
-
-  % need only volume of unique cells
-  vol = vol(f2r);
-  vol = vol(1:Mspecies:end);
-
-  % Generalized subdomains. To support more than one subdomain in 
-  % the models, the global expression "rdme_sdlevel" has to 
-  % be defined.
-
-  sd = ones(1,Ncells);
-  if isfield(fem,'globalexpr')
-      i = ismember(fem.globalexpr(1:2:end),'rdme_sdlevel');
-      [sdused,j]=max(i);
-      if sdused
-      temp=fem.globalexpr(2*j);
-      rdme_sdlevel = str2num(temp{1});
-      sd = zeros(1,Ncells);
-      if verbose>0
-        disp('Start xmeshinfo')
-      end
-      els = xmeshinfo(fem,'out','elements');
-      if verbose>0
-        disp('Done')
-        disp('Start xmeshinfo')
-      end
-      sdim = max(xmeshinfo(fem,'out','edim'));
-      if verbose>0
-        disp('Done')
-      end
-      for edim = sdim:-1:rdme_sdlevel
-          enodes = els{edim+1}.nodes;
-          if edim == 0
-              spoint = [0];
-          else
-              spoint = [zeros(edim,1) eye(edim)];
-          end
-          try
-              if verbose>0
-                disp('Start posteval')
-              end
-              rdme_sd = posteval(fem,'rdme_sd','spoint',spoint,'edim',edim);
-              if verbose>0
-                disp('Done')
-              end
-          catch
-              error(['To use more than une subdomain, you need to specify' ...
-              'the expression rdme_sd, see manual.'])
-          end
-          sd(enodes(:,rdme_sd.elind(1:edim+1:end))) = rdme_sd.d;
-      end
-      sd = sd(dofs.nodes(1:Mspecies:end));
-      end
-  end
-end  
-
-% Create URDME 1.2 structure
-umod = struct();
-umod.version = '1.2';
-if(isjava(fem))
-  umod.mph     = '4.x';
-else
-  umod.mph     = '3.5a';	
+  % sd is currently ordered according to comsol nodes. We need to
+  % permute it to be ordered according to comsol dofs (like everything
+  % else).
+  sd = sd(xmi.dofs.nodes(1:Mspecies:end)+1);
+  % add 1 to every index as 4.2 indices start from 0
 end
 
-umod.tspan   = tspan;
-umod.u0      = u0;
-umod.D       = D';
-umod.vol     = vol;
-umod.sd      = sd;
-umod.Ncells  = Ncells;
-umod.sopts   = [];
-umod.comsol  = fem;
+% In the Comsol API the species' names are refered to as
+% "modelID.speciesName", e.g., "mod1.MinDcytATP". The following code
+% removes the prefix.
+tags = fem.modelNode.tags;
+species = xmi.fieldnames;
 
-	
+for i = 1:size(xmi.fieldnames)
+  if strfind(xmi.fieldnames{i},[char(tags(1)) '.']) == 1
+    fullName = char(xmi.fieldnames{i});
+    species{i} = fullName(size(char(tags(1)),2)+2:end);
+  end
+end
+
+% finalize the Comsol-part of the URDME structure
+umod = struct('D',D', ...
+              'vol',vol, ...
+              'sd',sd, ...
+              'comsol',fem);
+
+%-------------------------------------------------------------------------
+function l_info(level,lim,msg)
+%L_INFO Display information.
+%   L_INFO(LEVEL,LIM,MSG) Displays the message MSG whenever LEVEL >=
+%   LIM.
+
+if level >= lim, fprintf(msg); end
+
+%-------------------------------------------------------------------------
--- urdme/comsol/urdme2comsol.m
+++ urdme/comsol/urdme2comsol.m
@@ -1,136 +1,76 @@
-function umod = urdme2comsol(umod,U,tspan,verbose)
-%URDME2COMSOL Update comsol model in umod.comsol with result matrix U
-%   UMOD = URDME2COMSOL(UMOD,U,VERBOSE) Inserts solution matrix U into
-%   Umod.comsol
+function umod = urdme2comsol(umod,verbose)
+%URDME2COMSOL Update .comsol-field with URDME result.
+%   UMOD = URDME2COMSOL(UMOD,VERBOSE) inserts the trajectory
+%   (UMOD.U,UMOD.tspan) into UMOD.comsol under the verbosity VERBOSE.
 %
-%   --INPUT--
-%   UMOD - Structure containing URDME simulation parameters and the
-%       comsol model in umod.comsol.
-%   U - Solution matrix produced by URDME core. U are in absolute integers
-%       (number of species), not concentration. URDME2COMSOL turns values to 
-%       consentration before saving to the model object. 
-%   tspan - Times span vector that matches the size of U. Defaults to umod.tspan.  
-%   VERBOSE - Silent execution
+%   The solution matrix U produced by URDME contains integers (number
+%   of species), not concentrations. URDME2COMSOL turns these values
+%   into concentration for handling by the Comsol API. On return
+%   UMOD.comsol.sol1 is the solution thus added which corresponds to
+%   input (UMOD.U,UMOD.tspan).
 %
-%   --OUTPUT--
-%   UMOD - Same as input but with U added to Solution 'sol1' of umod.cosmol
-%
-%   See also COMSOL2URDME, URDME.
+%   See also URDME, COMSOL2URDME.
 
-% P.Bauer       2012-08-30
-% V.Gerdin      2012-02-27 (rdme2mod)
-% A. Hellander  2010-05-04 (rdme2fem)
-% J. Cullhed    2008-08-06 (rdme2fem)
+% S. Engblom 2017-02-15 (Major revision, URDME 1.3, Comsol 5)
+% P.Bauer 2012-08-30 (urdme2comsol)
+% V.Gerdin 2012-02-27 (rdme2mod)
+% A. Hellander 2010-05-04
+% J. Cullhed 2008-08-06 (rdme2fem)
 
-if(isfield(umod,'comsol'))
-    is4x = isjava(umod.comsol);
+if nargin < 2, verbose = 0; end
+
+% extend mesh from model
+l_info(verbose,1,'Start mphxmeshinfo...');
+xmi = mphxmeshinfo(umod.comsol);
+l_info(verbose,1,' ...done.\n');
+
+% fundamentals
+nodes = xmi.nodes;
+[Mspecies,Ncells] = size(nodes.dofs);
+Ndofs = Mspecies*Ncells;
+
+% permutation of species
+if isfield(umod,'species')
+  fieldnames = umod.species;
 else
-    is4x = 0;
+  fieldnames = xmi.fieldnames;
 end
 
-if nargin < 3
-    tspan = umod.tspan;
+% fixed for Comsol 4.3
+modName = char(umod.comsol.modelNode.tags());
+if isempty(strfind(char(fieldnames(1)),modName))
+  fieldnames = strcat(modName,'.',fieldnames);
+end
+[foo,sp] = ismember(fieldnames,nodes.dofnames);
+isp(sp) = 1:numel(sp); % invert order
+
+% Scale U
+U = reshape(umod.U,Mspecies,Ncells,[]);
+
+% Change from absolute figures to concentration
+V = repmat(umod.vol(:)'*6.022e23,[Mspecies 1]);
+for i = 1:numel(umod.tspan)
+  U(:,:,i) = U(:,:,i)./V;
 end
 
-if nargin < 4
-    verbose = 0;
+% Finally reshape U in order of Dofs
+U = reshape(U(isp,:,:),Ndofs,[]);
+
+% Store U in model
+l_info(verbose,1,'Saving U to model...');
+for i = 1:numel(umod.tspan)
+  umod.comsol.sol('sol1').setU(i,U(:,i));
 end
-  
+umod.comsol.sol('sol1').setPVals(umod.tspan);
+umod.comsol.sol('sol1').createSolution;
+l_info(verbose,1,' ...done.\n');
 
-if is4x
-  %extend mesh from model
-  xmi = mphxmeshinfo(umod.comsol);
+%-------------------------------------------------------------------------
+function l_info(level,lim,msg)
+%L_INFO Display information.
+%   L_INFO(LEVEL,LIM,MSG) Displays the message MSG whenever LEVEL >=
+%   LIM.
 
-  % RESHAPE U TO FIT MODEL
+if level >= lim, fprintf(msg); end
 
-  % fundamentals
-  nodes = xmi.nodes;
-  [Mspecies,Ncells] = size(nodes.dofs);
-  Ndofs = Mspecies*Ncells;
-
-  % permutation of species
-  if isfield(umod,'species')
-      fieldnames = umod.species;
-  else
-      fieldnames = xmi.fieldnames;
-  end
-
-  % Fixed for Comsol 4.3
-  modName = char(umod.comsol.modelNode.tags());
-  if isempty(strfind(char(fieldnames(1)),modName))
-    fieldnames=strcat(modName,'.',fieldnames);
-  end
-  [foo,sp] = ismember(fieldnames,nodes.dofnames);
-  isp(sp) = 1:numel(sp); %invert order
-
-  % Scale U
-  U = reshape(U,Mspecies,Ncells,[]);
-
-  % Change from absolute figures to concentration
-  % suboptimal, but should work ok
-  vol = umod.vol;
-
-  V = repmat(vol(:)'*6.022e23,[Mspecies 1]);
- 
-  for i = 1:numel(tspan)
-      U(:,:,i) = U(:,:,i)./V;
-  end
-
-  % Finally reshape U in order of Dofs
-  U = reshape(U(isp,:,:),Ndofs,[]);
-
-  % Store U in model
-  if verbose>0
-    disp('Saving U to Model')
-  end
-  
-  for i=1:1:length(tspan)
-      umod.comsol.sol('sol1').setU(i,U(:,i));
-      if verbose>1
-        disp(sprintf('Saved %2.0f%%',i*100/length(tspan)))
-      end
-  end
-
-  umod.comsol.sol('sol1').setPVals(tspan);
-  umod.comsol.sol('sol1').createSolution;
-
-else
-
-  % extend mesh
-  xmesh = meshextend(umod.comsol);
-
-  % fundamentals
-  nodes = xmeshinfo(xmesh,'out','nodes');
-  [Mspecies,Ncells] = size(nodes.dofs);
-  Ndofs = Mspecies*Ncells;
-
-  % permutation of species
-  [foo,sp] = ismember(umod.comsol.equ.dim,nodes.names);
-  isp(sp) = 1:numel(sp);
-
-  % scale and permute back
-  U = reshape(U,Mspecies,Ncells,[]);
-
-  % suboptimal, but should work ok:
-  vol = umod.vol;
-  V = repmat(vol(:)'*6.022e23,[Mspecies 1]);
-  for i = 1:numel(tspan)
-    U(:,:,i) = U(:,:,i)./V;
-  end
-
-  % final shape
-  U = reshape(U(isp,:,:),Ndofs,[]);
-  
-  % Store U in model
-  if verbose>0
-    disp('Saving U to Model')
-  end
-
-  % final call
-  umod.comsol.sol = femsol(U, 'tlist', tspan);  
-  
-end
-
-if verbose>0
-  disp('Saving: Done')
-end
+%-------------------------------------------------------------------------
