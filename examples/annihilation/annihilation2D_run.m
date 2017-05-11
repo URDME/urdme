dcca4b97869b16b5ab9967885c66a64b15614595
Stefan Engblom <stefane@it.uu.se>
2017-05-11 13:43:50 +0200

    annihilation example revisited

--- examples/annihilation/annihilation2D_run.m
+++ examples/annihilation/annihilation2D_run.m
@@ -3,22 +3,63 @@
 %   when they meet. This example shows how one can set up a simple 2D
 %   simulation using the URDME interface to PDE Toolbox.
 
+% S. Engblom 2017-05-09 (Revision, rparse_inline)
 % S. Engblom 2017-02-21
 
-% generate URDME model
-clear umod
-[umod,G,P,E,T] = annihilation2D;
+%% (1) geometry and diffusion operator
 
-if ~exist('plotting_off','var') || ~plotting_off
-  figure(1), clf,
-  pdegplot(G,'subdomainlabels','on'), axis equal
+% scaling of reactions and diffusions
+R_const = 1;
+D_const = 0.1;
 
-  figure(2), clf,
-  pdemesh(P,E,T), axis equal
-end
+% build the PDE Toolbox model
+
+% create the geometry, here composed of three circles
+C1 = [1 0 0 1]';
+C2 = [1 -0.4 0 0.2]';
+C3 = [1 0.4 0 0.2]';
+gd = [C1 C2 C3];
+sf = 'C1+C2+C3';
+ns = char('C1','C2','C3')';
+G = decsg(gd,sf,ns);
+
+% create the mesh
+[P,E,T] = initmesh(G,'hmax',0.075);
+
+% assemble the diffusion part
+umod = pde2urdme(P,T,{D_const D_const});
+
+% this "rounding" will include only the nodes truly inside C2, C3:
+umod.sd(umod.sd < 1.5) = 1;
+umod.sd(umod.sd > 2.5) = 3;
+umod.sd = round(umod.sd);
+
+%% (2) reactions
+
+% @ --> A (sd == 1), @ --> B (sd == 3), A+B --> @ (everywhere)
+k_creat = R_const*200.0;
+k_react = R_const*1.0;
+[K,I,umod.N,umod.G] = rparse_inline( ...
+    {'@ > k_creat > A', ...
+     '@ > k_creat > B', ...
+     'A+B > k_react > @'}, ...
+    {'A' 'B'},{'k_creat' k_creat 'k_react' k_react});
+
+% specify in what subdomains reactions are turned off
+S = [2 1 0; ...
+     3 2 0];
+S = sparse(S);
+umod.solverargs = {'K' K 'I' I 'S' S};
+
+%% (3) run the model
+
+% initial conditions
+Mspecies = size(umod.N,1);
+Ncells = numel(umod.vol);
+umod.u0 = zeros(Mspecies,Ncells);
 
 % simulate
-umod = urdme(umod,'seed',20170221);
+umod = urdme(umod,'tspan',0:1000-1,'seed',20170221);
 umod = urdme2pde(umod);
 
 % compute temporal means
@@ -27,6 +68,12 @@ mean_B = mean(umod.pde.U(2,:,end/2:end),3);
 
 % visualize (can be turned off)
 if ~exist('plotting_off','var') || ~plotting_off
+  figure(1), clf,
+  pdegplot(G,'subdomainlabels','on'), axis equal
+
+  figure(2), clf,
+  pdemesh(P,E,T), axis equal
+  
   figure(3), clf
   pdesurf(umod.pde.P,umod.pde.T,mean_A');
   title('Mean A');
--- examples/annihilation/annihilation_run.m
+++ examples/annihilation/annihilation_run.m
@@ -4,15 +4,69 @@
 %   propensities are used, and how one can set up a simple 1D
 %   simulation.
 
+% S. Engblom 2017-05-09 (Revision, rparse_inline)
 % S. Engblom 2017-02-18 (Major revision, URDME 1.3, Comsol 5)
 % B. Drawert 2012
 
-% generate URDME model
+%% (1) reactions
+
+% scaling of reactions and diffusions
+R_const = 1;
+D_const = 1;
+
+% @ --> A (sd == 2), @ --> B (sd == 3), A+B --> @ (everywhere)
 clear umod
-umod = annihilation;
+k_creat = R_const*200.0;
+k_react = R_const*1.0;
+[K,I,umod.N,umod.G] = rparse_inline( ...
+    {'@ > k_creat > A', ...
+     '@ > k_creat > B', ...
+     'A+B > k_react > @'}, ...
+    {'A' 'B'},{'k_creat' k_creat 'k_react' k_react});
+
+% specify in what subdomains reactions are turned off
+S = [1 1 0; ...
+     3 2 0];
+S = sparse(S);
+umod.solverargs = {'K' K 'I' I 'S' S};
+
+%% (2) diffusion
+
+% length of domain
+L = 10;
+
+% discretization
+Ncells = 20;
+x = linspace(0,L,Ncells);
+dx = x(2)-x(1);
+umod.private.mesh = x; % (used to plot)
+umod.vol = dx^3*ones(Ncells,1);
+
+% 1D diffusion operator
+e = D_const*ones(Ncells,1)/dx^2;
+D = spdiags([e -2*e e],-1:1,Ncells,Ncells);
+
+% reflecting boundary
+D(1,1) = -e(1);
+D(end,end) = -e(end);
+
+% URDME ordering of species
+umod.D = kron(D,eye(2));
+
+% three subdomains
+umod.sd = ones(1,Ncells);
+umod.sd([1 end]) = [2 3];
+
+%% (3) run the model
+
+% initial conditions
+Mspecies = size(umod.N,1);
+umod.u0 = zeros(Mspecies,Ncells);
 
 % simulate
-umod = urdme(umod,'seed',20170219);
+umod = urdme(umod,'tspan',0:1000-1,'seed',20170219);
+
+%% (4) postprocessing
 
 % compute means
 mean_A = mean(umod.U(1:2:end,end/2:end),2)./umod.vol;
